b57ab2c3ed5ee86468fdc81bc1375fa594319a8d
Ra-1007 <thegamer4268@gmail.com>
2025-11-18 10:32:15 +0530

    PrioritySet

--- Task A/Deliver/src/main/java/com/andronicus/routeplanner/PrioritySorter.java
+++ Task A/Deliver/src/main/java/com/andronicus/routeplanner/PrioritySorter.java
@@ -1,11 +1,16 @@
-<project xmlns="http://maven.apache.org/POM/4.0.0"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://maven.apache.org/POM/4.0.0
-        http://maven.apache.org/xsd/maven-4.0.0.xsd">
+package com.andronicus.routeplanner;
 
-<modelVersion>4.0.0</modelVersion>
-<groupId>com.andronicus.routeplanner</groupId>
-<artifactId>SmartDeliveryRoutePlanner</artifactId>
-<version>1.0-SNAPSHOT</version>
+import java.util.*;
 
-</project>
+public class PrioritySorter {
+    public static List<DeliveryPoint> sortByPriority(List<DeliveryPoint> points) {
+        Map<String, Integer> priorityOrder = Map.of(
+                "high", 1,
+                "medium", 2,
+                "low", 3
+        );
+
+        points.sort(Comparator.comparingInt(p -> priorityOrder.get(p.priority)));
+        return points;
+    }
+}
